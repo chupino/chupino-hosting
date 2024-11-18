caf033253b7dcbdcc3a931297a618477587db0ec
chupino <mauricioescalante903@gmail.com>
2024-11-18 00:11:16 -0500

    first commit

--- node/src/utils/gamefileSettings.js
+++ node/src/utils/gamefileSettings.js
@@ -1,8 +1,9 @@
 const fs = require('fs').promises;
+const path = require('path');
 const {uploadFile} = require('../services/bucketService')
 const uploadMinecraftFiles = async () => {
     try{
-        const serverproperties = '../../config/gamefiles/server.properties';
+        const serverproperties = path.resolve(__dirname, '../../config/gamefiles/server.properties');
         const serverpropertiesContent = await fs.readFile(serverproperties, 'utf8');
         
         const fileUploaded = await uploadFile("server.properties",serverpropertiesContent,"game_settings_files/1")
