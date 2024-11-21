4c770c15681304f9fd114741927afe16ad7a4353
chupino <mauricioescalante903@gmail.com>
2024-11-21 00:12:14 -0500

    cors

--- node/src/app.js
+++ node/src/app.js
@@ -10,52 +10,7 @@ const {uploadMinecraftFiles} = require('./utils/gamefileSettings')
 const app = express();
 
 app.use(express.json())
-const getPublicIpAddress = async () => {
-  try {
-    const response = await axios.get('https://api.ipify.org?format=json');
-    return response.data.ip;
-  } catch (error) {
-    console.error('Error fetching public IP address:', error);
-    return '127.0.0.1';
-  }
-};
-
-const setupCors = async () => {
-  const publicIp = await getPublicIpAddress();
-  console.log(`Public IP Address: ${publicIp}`);
-
-  const allowedOrigins = [
-    `http://${publicIp}`,
-    'http://localhost',
-    'http://127.0.0.1'
-  ];
-
-  console.log('Allowed Origins for CORS:', allowedOrigins);
-
-  const corsOptions = {
-    origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    optionsSuccessStatus: 200
-  };
-
-  app.use(cors(corsOptions));
-
-  // Middleware para manejar solicitudes preflight
-  app.options('*', (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.sendStatus(204); // Respuesta vacía para OPTIONS
-  });
-};
-
-
-setupCors();
+app.use(cors())
 
 
 //Inicializando archivos de configuracion
